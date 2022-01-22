b7a4c32d25ec90b5c79a968822198addfa52f828
Umut <u.c.erturk@gmail.com>
2022-01-22 18:25:30 +0300

    Created MVC Roots manager

---
+++ Assets/Scripts/MVC/Runtime/Root/RootsManager.cs
@@ -0,0 +1,25 @@
+﻿namespace MVC.Runtime.Root
+{
+    public class RootsManager
+    {
+        private static RootsManager _instance;
+        public static RootsManager Instance
+        {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new RootsManager();
+                    _instance.Initialize();
+                }
+
+                return _instance;
+            }
+        }
+
+        public void Initialize()
+        {
+            
+        }
+    }
+}
