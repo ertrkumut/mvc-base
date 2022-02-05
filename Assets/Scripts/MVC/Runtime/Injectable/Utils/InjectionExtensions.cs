7601b0ecd51477253f08e0a04b86d4a0ada3d1e7
Umut <u.c.erturk@gmail.com>
2022-02-05 10:52:34 +0300

    Refactoring mediator injection

--- Assets/Scripts/MVC/Runtime/Injectable/Utils/InjectionExtensions.cs
+++ Assets/Scripts/MVC/Runtime/Injectable/Utils/InjectionExtensions.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 using System.Reflection;
 using MVC.Runtime.Contexts;
@@ -23,26 +24,20 @@ namespace MVC.Runtime.Injectable.Utils
         private static void InjectFields(object mediator, object view, IContext context)
         {
             var viewType = view.GetType();
-            var mediatorType = mediator.GetType();
-            
-            var injectableFields = mediatorType.GetFields(BindingFlags.Instance | 
-                                                          BindingFlags.Public | 
-                                                          BindingFlags.NonPublic)
-                .Where(x => x.GetCustomAttributes(typeof(InjectAttribute)).ToList().Count != 0)
-                .ToList();
+
+            var injectableFields = GetFieldInfoList(mediator);
             
             foreach (var injectableFieldInfo in injectableFields)
             {
                 var fieldType = injectableFieldInfo.FieldType;
-                var injectAttribute = injectableFieldInfo.GetCustomAttributes(typeof(InjectAttribute)).ToList()[0] as InjectAttribute;
-                
+
                 if (fieldType == viewType)
                 {
                     injectableFieldInfo.SetValue(mediator, view);
                 }
                 else
                 {
-                    var injectionValue = context.GetInjectedObject(fieldType, injectAttribute.Name);
+                    var injectionValue = context.GetInjectedObject(injectableFieldInfo);
                     if (injectionValue == null)
                     {
                         Debug.LogError("Injection Failed! There is no injected property in container! \n ViewType: " + viewType + "\n Injection Type: " + fieldType);
@@ -57,45 +52,64 @@ namespace MVC.Runtime.Injectable.Utils
         private static void InjectProperties(object mediator, object view, IContext context)
         {
             var viewType = view.GetType();
-            var mediatorType = mediator.GetType();
+
+            var injectablePropertyList = GetPropertyInfoList(mediator);
             
-            var injectableFields = mediatorType.GetProperties(BindingFlags.Instance | 
-                                                          BindingFlags.Public | 
-                                                          BindingFlags.NonPublic)
-                .Where(x => x.GetCustomAttributes(typeof(InjectAttribute)).ToList().Count != 0)
-                .ToList();
-            
-            foreach (var injectableFieldInfo in injectableFields)
+            foreach (var injectableProperty in injectablePropertyList)
             {
-                var fieldType = injectableFieldInfo.PropertyType;
-                var injectAttribute = injectableFieldInfo.GetCustomAttributes(typeof(InjectAttribute)).ToList()[0] as InjectAttribute;
-                
+                var fieldType = injectableProperty.PropertyType;
+
                 if (fieldType == viewType)
                 {
-                    injectableFieldInfo.SetValue(mediator, view);
+                    injectableProperty.SetValue(mediator, view);
                 }
                 else
                 {
-                    var injectionValue = context.GetInjectedObject(fieldType, injectAttribute.Name);
+                    var injectionValue = context.GetInjectedObject(injectableProperty);
                     if (injectionValue == null)
                     {
                         Debug.LogError("Injection Failed! There is no injected property in container! \n ViewType: " + viewType + "\n Injection Type: " + fieldType);
                         continue;
                     }
                     
-                    injectableFieldInfo.SetValue(mediator, injectionValue);
+                    injectableProperty.SetValue(mediator, injectionValue);
                 }
             }
         }
 
-        private static object GetInjectedObject(this IContext context, Type injectionType, string name = "")
+        private static List<FieldInfo> GetFieldInfoList(object instance)
         {
+            var injectableFields = instance.GetType().GetFields(BindingFlags.Instance | 
+                                                                BindingFlags.Public | 
+                                                                BindingFlags.NonPublic)
+                .Where(x => x.GetCustomAttributes(typeof(InjectAttribute)).ToList().Count != 0)
+                .ToList();
+
+            return injectableFields;
+        }
+
+        private static List<PropertyInfo> GetPropertyInfoList(object instance)
+        {
+            var injectableProperties = instance.GetType().GetProperties(BindingFlags.Instance | 
+                                       BindingFlags.Public | 
+                                       BindingFlags.NonPublic)
+                .Where(x => x.GetCustomAttributes(typeof(InjectAttribute)).ToList().Count != 0)
+                .ToList();
+
+            return injectableProperties;
+        }
+        
+        private static object GetInjectedObject(this IContext context, MemberInfo memberInfo)
+        {
+            var injectionType = memberInfo.DeclaringType;
+            var injectAttribute = memberInfo.GetCustomAttributes(typeof(InjectAttribute)).ToList()[0] as InjectAttribute;
+            
             var injectionBinder = context.InjectionBinder;
             var crossContextInjectionBinder = context.CrossContextInjectionBinder;
             
-            var injectionValue = injectionBinder.GetInstance(injectionType, name);
+            var injectionValue = injectionBinder.GetInstance(injectionType, injectAttribute.Name);
             if (injectionValue == null)
-                injectionValue = crossContextInjectionBinder.GetInstance(injectionType, name);
+                injectionValue = crossContextInjectionBinder.GetInstance(injectionType, injectAttribute.Name);
 
             return injectionValue;
         }
