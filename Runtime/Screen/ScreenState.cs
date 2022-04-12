4423e3d7bfcdcd68bb610231667bfbc35f9590cb
Umut Erturk <u.c.erturk@gmail.com>
2022-04-12 16:29:12 -0700

    ScreenState enum created

---
+++ Runtime/Screen/ScreenState.cs
@@ -0,0 +1,11 @@
+namespace MVC.Screen
+{
+    public enum ScreenState
+    {
+        None = 0,
+        InPool = 5,
+        InUse = 10,
+        InOpeningAnimation = 15,
+        InClosingAnimation = 20
+    }
+}
