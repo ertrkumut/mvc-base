2bb4860772848273ece6be27bcd0f09147b20f30
Umut Erturk <u.c.erturk@gmail.com>
2022-04-21 17:11:40 -0700

    Show Screen

--- Runtime/Screen/ScreenModel.cs
+++ Runtime/Screen/ScreenModel.cs
@@ -1,5 +1,9 @@
+using System;
 using System.Collections.Generic;
 using MVC.Runtime.Injectable.Attributes;
+using MVC.Runtime.Screen.Pool;
+using MVC.Runtime.Screen.View;
+using UnityEngine;
 
 namespace MVC.Runtime.Screen
 {
@@ -7,10 +11,15 @@ namespace MVC.Runtime.Screen
     {
         private Dictionary<int, IScreenManager> _screenManagerDict;
 
+        private List<ScreenDataContainer> _screenDataContainerPool;
+
+        [Inject] protected IScreenPoolController _screenPoolController;
+        
         [PostConstruct]
         protected void PostConstruct()
         {
             _screenManagerDict = new Dictionary<int, IScreenManager>();
+            _screenDataContainerPool = new List<ScreenDataContainer>();
         }
 
         public void RegisterScreenManager(ScreenManager screenManager)
@@ -24,11 +33,60 @@ namespace MVC.Runtime.Screen
             if(_screenManagerDict.ContainsKey(screenManager.ManagerIndex))
                 _screenManagerDict.Remove(screenManager.ManagerIndex);
         }
+
+        public IScreenDataContainer NewScreen(System.Enum screenType)
+        {
+            var dataContainer = GetAvailableScreenDataContainer();
+            dataContainer.ScreenType = screenType;
+            return dataContainer;
+        }
+
+        internal TScreenType CreateOrGetScreen<TScreenType>(ScreenDataContainer screenDataContainer)
+            where TScreenType : MonoBehaviour, IScreenBody
+        {
+            var screenManager = GetScreenManager(screenDataContainer.ManagerIndex);
+
+            var availableScreen = _screenPoolController.GetScreenFromPool(screenDataContainer.ScreenType);
+            
+            _screenDataContainerPool.Add(screenDataContainer);
+            screenDataContainer.Dispose();
+            
+            return default;
+        }
+
+        public IScreenManager GetScreenManager(int managerId)
+        {
+            if (!_screenManagerDict.ContainsKey(managerId))
+            {
+                throw new Exception("There is no screen manager!! - Id: " + managerId);
+            }
+
+            return _screenManagerDict[managerId];
+        }
+        
+        private ScreenDataContainer GetAvailableScreenDataContainer()
+        {
+            ScreenDataContainer dataContainer;
+
+            if (_screenDataContainerPool.Count > 0)
+            {
+                dataContainer = _screenDataContainerPool[0];
+                _screenDataContainerPool.Remove(dataContainer);
+            }
+            else
+                dataContainer = new ScreenDataContainer(this);
+            
+            return dataContainer;
+        }
     }
 
     public interface IScreenModel
     {
         void RegisterScreenManager(ScreenManager screenManager);
         void UnRegisterScreenManager(ScreenManager screenManager);
+
+        IScreenDataContainer NewScreen(System.Enum screenType);
+
+        IScreenManager GetScreenManager(int managerId);
     }
 }
