4904be1a2ca96a9e7335ecb409f958a22621e542
Umut Erturk <u.c.erturk@gmail.com>
2022-09-22 07:22:45 -0700

    Screen body bug fix

--- Runtime/Screen/View/ScreenBody.cs
+++ Runtime/Screen/View/ScreenBody.cs
@@ -30,6 +30,7 @@ namespace MVC.Runtime.Screen.View
         protected virtual void ScreenClosed()
         {
             ScreenState = ScreenState.InPool;
+            gameObject.SetActive(false);
             OnScreenClosed?.Invoke();
             OnReturnToPool();
         }
